036d23b4599186d42d851ee12a5acc13b035ce5e
Bilab Thapa <thapabilab9@gmail.com>
2022-05-20 20:37:32 +0545

    validator and bottom navbar

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -8,6 +8,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
@@ -17,138 +18,160 @@ class _LoginState extends State<Login> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Center(
-            child: Column(
-              children: [
-                SizedBox(height: height * 0.07),
-                Text(
-                  'Sign In',
-                  style: TextStyle(
-                      color: Colors.deepOrange,
-                      fontSize: 38,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 50),
-                Container(
-                  height: height * 0.4,
-                  width: width * 0.8,
-                  decoration: BoxDecoration(
-                    color: Color(0xffE0FBFC),
-                    borderRadius: BorderRadius.circular(40),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(height: height * 0.07),
+                  Text(
+                    'Sign In',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontSize: 38,
+                        fontWeight: FontWeight.bold),
                   ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: height * 0.05),
-                      Row(
-                        children: [
-                          SizedBox(width: width * 0.02),
-                          CircleAvatar(
-                            backgroundColor: Color(0xffFF5733),
-                            child: Icon(
-                              Icons.person,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(width: width * 0.04),
-                          Expanded(
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                hintText: 'Email',
+                  const SizedBox(height: 50),
+                  Container(
+                    height: height * 0.4,
+                    width: width * 0.8,
+                    decoration: BoxDecoration(
+                      color: Color(0xffE0FBFC),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(height: height * 0.05),
+                        Row(
+                          children: [
+                            SizedBox(width: width * 0.02),
+                            CircleAvatar(
+                              backgroundColor: Color(0xffFF5733),
+                              child: Icon(
+                                Icons.person,
+                                color: Colors.white,
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: height * 0.04),
-                      Row(
-                        children: [
-                          SizedBox(width: width * 0.02),
-                          CircleAvatar(
-                            backgroundColor: Color(0xffFF5733),
-                            child: Icon(
-                              Icons.lock,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(width: width * 0.04),
-                          Expanded(
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                hintText: 'Password',
+                            SizedBox(width: width * 0.04),
+                            SizedBox(
+                              width: width * 0.6,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'Email',
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter email';
+                                  }
+                                  return null;
+                                },
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: height * 0.07),
-                      SizedBox(
-                        height: height * 0.07,
-                        width: width * 0.7,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).pushNamed('/home');
-                          },
-                          child: Text(
-                            'Sign In',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.indigo,
-                            shape: (RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(40.0),
-                              side: BorderSide(color: Colors.indigo),
-                            )),
+                          ],
+                        ),
+                        SizedBox(height: height * 0.04),
+                        Row(
+                          children: [
+                            SizedBox(width: width * 0.02),
+                            CircleAvatar(
+                              backgroundColor: Color(0xffFF5733),
+                              child: Icon(
+                                Icons.lock,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: width * 0.04),
+                            SizedBox(
+                              width: width * 0.6,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'Password',
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter password';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: height * 0.04),
+                        SizedBox(
+                          height: height * 0.07,
+                          width: width * 0.7,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                Navigator.of(context).pushNamed('/home');
+                              } else {
+                                return null;
+                              }
+                            },
+                            child: Text(
+                              'Sign In',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.indigo,
+                              shape: (RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40.0),
+                                side: BorderSide(color: Colors.indigo),
+                              )),
+                            ),
                           ),
                         ),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/forgot');
+                    },
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        color: Colors.indigo,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/register');
+                    },
+                    child: Text(
+                      'New around here? Register',
+                      style: TextStyle(
+                        color: Colors.indigo,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: height * 0.04),
+                  Text(
+                    'Login with',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: height * 0.04),
+                  Row(
+                    children: [
+                      SizedBox(width: width * 0.3),
+                      CircleAvatar(
+                        child: Icon(Icons.facebook),
+                      ),
+                      SizedBox(width: width * 0.2),
+                      CircleAvatar(
+                        child: Icon(Icons.mail_rounded),
                       ),
                     ],
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/forgot');
-                  },
-                  child: Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                      color: Colors.indigo,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/register');
-                  },
-                  child: Text(
-                    'New around here? Register',
-                    style: TextStyle(
-                      color: Colors.indigo,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-                SizedBox(height: height * 0.04),
-                Text(
-                  'Login with',
-                  style: TextStyle(
-                      color: Colors.deepOrange,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: height * 0.04),
-                Row(
-                  children: [
-                    SizedBox(width: width * 0.3),
-                    CircleAvatar(
-                      child: Icon(Icons.facebook),
-                    ),
-                    SizedBox(width: width * 0.2),
-                    CircleAvatar(
-                      child: Icon(Icons.mail_rounded),
-                    ),
-                  ],
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
